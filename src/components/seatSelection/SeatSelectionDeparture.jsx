a4c29234ea3b66c94600ccdeecdd00b021f96204
diana pinzon <diaphi@hotmail.com>
2023-05-16 22:59:32 -0500

    creación página Selección de asientos

--- src/components/seatSelection/SeatSelectionDeparture.jsx
+++ src/components/seatSelection/SeatSelectionDeparture.jsx
@@ -1,12 +1,182 @@
-import React from 'react';
-
+import React from "react";
+import {
+  StyleBottonDeparture,
+  StyledContainer,
+  StyledDate,
+} from "../departureFlight/StylesDeparture";
+import {
+  Aisle,
+  Button,
+  Column,
+  Column2,
+  ReferenSeat,
+  Row,
+  SeatSelect,
+  Subtittle,
+} from "./StyleSeatSelectionDeparture";
 
 const SeatSelectionDeparture = () => {
-    return (
-        <>
-       <h1>Seat Selection Departure</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <StyledDate>
+        <StyledContainer>
+          <h1>Vuelo de Salida</h1>
+          <StyleBottonDeparture>Cambiar vuelo</StyleBottonDeparture>
+        </StyledContainer>
+        <h2>Jueves 20 jul 2023</h2>
+        <h4>Medellín, Colombia(MDE)</h4>
+        <p>Selecciona tus asientos</p>
+      </StyledDate>
+      <ReferenSeat>
+        <Column>
+          <Row>
+            <Aisle>A</Aisle>
+            <Aisle>B</Aisle>
+            <Aisle>C</Aisle>
+          </Row>
+        </Column>
+        <Column>
+          <Aisle></Aisle>
+        </Column>
+        <Column>
+          <Row>
+            <Aisle>D</Aisle>
+            <Aisle>E</Aisle>
+            <Aisle>F</Aisle>
+          </Row>
+        </Column>
+      </ReferenSeat>
+      <Subtittle><h5>Salida Segura</h5></Subtittle>
+      <SeatSelect>
+        <Column>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+        </Column>
+          <Column2>
+            <Aisle>1</Aisle>
+            <Aisle>2</Aisle>
+            <Aisle>3</Aisle>
+            <Aisle>4</Aisle>
+            <Aisle>5</Aisle>
+            
+          </Column2>
+        <Column>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+        </Column>
+      </SeatSelect>
+      <Subtittle><h5>Estándar</h5></Subtittle>
+      <SeatSelect>
+        <Column>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+        </Column>
+          <Column2>
+            <Aisle>6</Aisle>
+            <Aisle>7</Aisle>
+            <Aisle>8</Aisle>
+            <Aisle>9</Aisle>
+            <Aisle>10</Aisle>
+          </Column2>
+        <Column>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+          <Row>
+            <Button></Button>
+            <Button></Button>
+            <Button></Button>
+          </Row>
+        </Column>
+      </SeatSelect>
+    </>
+  );
+};
 
 export default SeatSelectionDeparture
---
+++ src/components/seatSelection/StyleSeatSelectionDeparture.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const SeatSelect = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+  margin-bottom: 7px;
+`;
+
+export const ReferenSeat = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+  margin-bottom: 7px;
+`;
+
+export const Subtittle = styled.div`
+  margin-bottom: 10px;
+
+`;
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 200px;
+`;
+
+export const Column2 = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Aisle = styled.div`
+  height: 50px;
+  width: 50px;
+  color: black;
+  background-color: white;
+  border: 1px solid gray;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+  padding: 20px;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Button = styled.button`
+  height: 50px;
+  width: 50px;
+  background-color: #b9b9b9;
+  border: none;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  margin-right: 10px;
+`;
